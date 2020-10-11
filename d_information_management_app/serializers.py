d1b3ca153539aece16df98bd262b1e1f6b07308b
Jeison Ortiz <jesonog@gmail.com>
2020-10-11 11:14:58 -0500

    Commit para actualizar el archivo serializers.py

--- d_information_management_app/serializers.py
+++ d_information_management_app/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .views import Pais
+from .models import Pais
 
-class paisSerializer(serializers.ModelSerializer):
-    class META():
-        model=Pais
-        campos=('nombre')
+
+class PaisSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    nombre = serializers.CharField()
+
+    def create(self, validate_data):
+        instance = Pais()
+        instance.nombre = validate_data.get('nombre')
+        instance.save()
+        return instance
+    
+    def validate_username(self, data):
+        paises = Pais.objects.filter(nombre = data)
+        if len(paises) != 0:
+            raise serializers.ValidationError("Este nombre de pais ya existe, Ingrese uno nuevo")
+        else:
+            return data
